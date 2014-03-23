413b4444ce48ab9af2517fa4f65416e4dad907b4
Merblr <merblr@live.com>
2014-03-23 23:53:23 +0800

    Add routes

--- app/routes.php
+++ app/routes.php
@@ -1,17 +1,43 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
+Route::pattern('id', '[0-9]+');
+Route::pattern('username', '[A-Z0-9a-z]+');
+Route::pattern('reply', 'reply|r');
 
-Route::get('/', function()
+Route::group(['prefix'=>'admin'], function()
 {
-	return View::make('hello');
+    Route::get('/', 'Admin\HomeController@index');
+    Route::get('login', 'Admin\SessionController@create');
+    Route::get('users', 'Admin\UserController@index');
+    Route::get('topics', 'Admin\TopicController@index');
+    Route::get('categories', 'Admin\CategoryController@index');
+});
+
+Route::get('login', 'SessionController@create');
+Route::get('logout', 'SessionController@logout');
+Route::post('sessions/store', 'SessionController@store');
+Route::delete('logout', 'SessionController@destroy');
+
+Route::get('signup', 'UserController@create');
+Route::get('users', 'UserController@index');
+Route::post('users/store', 'UserController@store');
+Route::get('u/{username}', 'UserController@show');
+
+Route::get('/', 'TopicController@index');
+Route::get('topic/new', 'TopicController@create');
+Route::get('topic/{id}', 'TopicController@show');
+Route::get('topic/{id}', 'TopicController@edit');
+Route::post('topic/store', 'TopicController@store');
+Route::put('topic/{id}', 'TopicController@update');
+Route::delete('topic/{id}', 'TopicController@destroy');
+
+Route::get('u/{username}/replies', 'UserController@show');
+Route::post('{reply}/store', 'ReplyController@store');
+
+Route::get('{reply}/{id}', 'ReplyController@show');
+Route::put('{reply}/{id}', 'ReplyController@update');
+
+Route::group(['domain'=>'ghost.nhn.io'], function()
+{
+
 });
