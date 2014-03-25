bf83b1015260dfe5b66d3088a4f3acbd0c751252
Merblr <merblr@live.com>
2014-03-25 22:33:44 +0800

    Add entypo

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -3,8 +3,27 @@
 <head>
     <meta charset="utf-8">
     <title>{{ $title or 'Hello, Laravel.' }}</title>
+    {{ HTML::style('assets/application.css') }}
 </head>
 <body>
+<div class="navbar">
+    <div class="inner">
+        <div id="logo"></div>
+        <div class="options pull_right">
+            <a href=""><span class="icon-login"></span></a>
+            <a href=""><span class="icon-signup"></span></a>
+            <a href=""><span class="icon-logout"></span></a>
+            <a href=""><span class="icon-user"></span></a>
+            <a href=""><span class="icon-users"></span></a>
+            <a href=""><span class="icon-logout"></span></a>
+            <a href=""><span class="icon-home"></span></a>
+        </div>
+    </div>
+</div>
+<div class="master">
     @yield('main')
+    <div class="sidebar"></div>
+</div>
+<div class="footer"></div>
 </body>
 </html>
--- app/views/topics/index.blade.php
+++ app/views/topics/index.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.master')
 
 @section('main')
-Hello, Laravel.
+
+@stop
+
+@section('sidebar')
+
 @stop
