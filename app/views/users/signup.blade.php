203e06f3a90d33f931e872daf83bd31d97851c2b
Merblr <merblr@live.com>
2014-03-28 23:40:19 +0800

    Add submit button

--- app/views/users/signup.blade.php
+++ app/views/users/signup.blade.php
@@ -2,11 +2,12 @@
 
 @section('main')
     <div class="boxify">
-        {{ Form::open() }}
+        {{ Form::open(['url'=>'users/store']) }}
             <ul class="signup">
                 {{ Form::userText('username', Input::old('username'), ['label'=>Lang::get('locale.username')]) }}
                 {{ Form::userText('email', Input::old('email'), ['label'=>Lang::get('locale.email')]) }}
                 {{ Form::userPassword('password', ['label'=>Lang::get('locale.password')]) }}
+                {{ Form::submit(Lang::get('locale.signup'), ['class'=>'btn normal']) }}
             </ul>
         {{ Form::close() }}
     </div>
