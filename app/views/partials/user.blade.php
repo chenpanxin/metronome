8a350a5d6d30588e4195ace4f1450bfdbd27d446
Merblr <merblr@live.com>
2014-03-29 16:02:42 +0800

    Add logout icon

--- app/views/partials/user.blade.php
+++ app/views/partials/user.blade.php
@@ -1,5 +1,6 @@
 @if (Auth::check())
     <a href=""><span class="icon-user"></span></a>
+    <a href="{{ URL::to('logout') }}"><span class="icon-logout"></span></a>
 @else
     <a href="{{ URL::to('login') }}">{{ Lang::get('locale.login') }}</a>
     <a href="{{ URL::to('signup') }}">{{ Lang::get('locale.signup') }}</a>
