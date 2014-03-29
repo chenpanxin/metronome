a8d70f61401f395489037575141b26ddab1302aa
Merblr <merblr@live.com>
2014-03-29 22:28:21 +0800

    Fix profile view

--- app/views/users/profile/edit.blade.php
+++ app/views/users/profile/edit.blade.php
@@ -3,10 +3,26 @@
 @section('main')
     <div class="boxify">
         {{ Form::open(['url'=>'']) }}
-            <ul class="">
+            <ul class="profile">
                 {{ HTML::group([
                     Form::label('nickname'),
                     Form::text('nickname')
+                ]) }}
+                {{ HTML::group([
+                    Form::label('location'),
+                    Form::text('location')
+                ]) }}
+                {{ HTML::group([
+                    Form::label('company'),
+                    Form::text('company')
+                ]) }}
+                {{ HTML::group([
+                    Form::label('contact_email'),
+                    Form::text('contact_email')
+                ]) }}
+                {{ HTML::group([
+                    Form::label('biography'),
+                    Form::textarea('biography', '', ['rows'=>4])
                 ]) }}
             </ul>
         {{ Form::close() }}
