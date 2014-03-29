6ae431ad710eda125b48d62856625853e83eaf2d
Merblr <merblr@live.com>
2014-03-29 11:59:16 +0800

    Add signup

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -2,8 +2,27 @@
 
 class UserController extends BaseController {
 
+    public function __construct()
+    {
+
+    }
+
     public function create()
     {
         return View::make('users.signup');
+    }
+
+    public function store()
+    {
+        $user = new User;
+        $user->email = Input::get('email');
+        $user->username = Input::get('username');
+        $user->password = Hash::make(Input::get('password'));
+        $user->avatar_url = '';
+        if ($user->save()) {
+            Auth::login($user);
+            return Redirect::to('/');
+        }
+        return Redirect::back();
     }
 }
