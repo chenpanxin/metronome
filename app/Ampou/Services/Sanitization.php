a4b5a05094c26afb659f9780fb8251adedd1c7b2
Merblr <merblr@live.com>
2014-04-26 19:13:55 +0800

    Add HTMLPurifier

--- app/Ampou/Services/Sanitization.php
+++ app/Ampou/Services/Sanitization.php
@@ -1,5 +1,23 @@
-<?php namespace Ampou\Services\Sanitization;
+<?php namespace Ampou\Services;
+
+use HTMLPurifier;
 
 class Sanitization {
 
+    protected $purifier;
+
+    public function __construct()
+    {
+        $this->purifier = new HTMLPurifier();
+    }
+
+    public function sanitize($html)
+    {
+        return $this->purifier->purify($html);
+    }
+
+    public static function make($html)
+    {
+        return (new static())->sanitize($html);
+    }
 }
