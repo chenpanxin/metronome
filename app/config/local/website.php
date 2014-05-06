a0d175091da648fb932b3e79893e18aaf3a5a189
Merblr <merblr@live.com>
2014-05-06 09:11:38 +0800

    Fix config

--- app/config/local/website.php
+++ app/config/local/website.php
@@ -1,17 +1,10 @@
 <?php
 
 return array(
-
     'logo'        => 'laravel',
-
     'version'     => 'beta',
-
     'title'       => 'laravel',
-
     'keywords'    => 'laravel',
-
     'description' => 'laravel',
-
     'copyright'   => '© 2014 Application · All rights reserved.',
-
 );
--- app/config/website.php
+++ app/config/website.php
@@ -1,19 +1,15 @@
 <?php
 
 return array(
-
-    'logo'        => 'website logo',
-
-    'version'     => 'website version',
-
-    'author'      => 'website author',
-
-    'title'       => 'website title',
-
-    'keywords'    => 'website keywords',
-
-    'description' => 'website description',
-
-    'copyright'   => 'website copyright',
-
+    'logo'         => 'website logo',
+    'version'      => 'website version',
+    'author'       => 'website author',
+    'title'        => 'website title',
+    'keywords'     => 'website keywords',
+    'description'  => 'website description',
+    'copyright'    => 'website copyright',
+    'announcement' => 'website announcement',
+    'owners'       => array(),
+    'promos'       => array(),
+    'links'        => array(),
 );
