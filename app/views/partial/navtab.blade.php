d849bae4a4352a67c41114d2815ff403bff76d31
merblr <merblr@live.com>
2014-07-15 22:17:09 +0800

    Fix navbar

--- app/views/partial/navtab.blade.php
+++ app/views/partial/navtab.blade.php
@@ -1,7 +1,7 @@
 <ul>
     @if (Auth::check())
         <li><a href="{{ URL::to('notification') }}"><span class="icon-mail"></span></a></li>
-        <li><a href="/#" class="user-opt">{{ HTML::image('assets/avatar.jpg') }}</a></li>
+        <li><a href="/#" class="user-opt">{{ HTML::image('assets/avatar.jpg') }}<b>me2r</b></a></li>
     @else
         <li><a href="{{ URL::to('login') }}">{{ Lang::get('locale.login') }}</a></li>
         <li><a href="{{ URL::to('signup') }}">{{ Lang::get('locale.signup') }}</a></li>
